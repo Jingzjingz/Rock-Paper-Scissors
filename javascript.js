2627061bf425c265b9ba7640950a46b45d1301df
Jingzjingz <138029350+Jingzjingz@users.noreply.github.com>
2023-07-10 17:35:57 +0700

    .

--- javascript.js
+++ javascript.js
@@ -9,33 +9,56 @@ function getComputerChoice(){
     } 
 }
 
-//console.log(getComputerChoice());
+let score = 0
+let comScore = 0
 
 function playRound(playerSelection, computerSelection){
     if ((playerSelection.toUpperCase()) === (computerSelection.toUpperCase())){
         return "It's a draw!";
     } else if ((playerSelection.toUpperCase() === "ROCK") && (computerSelection.toUpperCase() === "SCISSORS")){
+        score += 1;
         return "You Win! Rock beats Scissors";
     } else if ((playerSelection.toUpperCase() === "ROCK") && (computerSelection.toUpperCase() === "PAPER")){
+        comScore += 1;
         return "You Lose! Paper beats Rock";
     } else if ((playerSelection.toUpperCase() === "PAPER") && (computerSelection.toUpperCase() === "ROCK")){
+        score += 1;
         return "You Win! Paper beats Rock";
     } else if ((playerSelection.toUpperCase() === "PAPER") && (computerSelection.toUpperCase() === "SCISSORS")){
+        comScore += 1;
         return "You Lose! Scissors beats Paper";
     } else if ((playerSelection.toUpperCase() === "SCISSORS") && (computerSelection.toUpperCase() === "ROCK")){
+        comScore += 1;
         return "You Lose! Rock beats Scissors";
     } else if ((playerSelection.toUpperCase() === "SCISSORS") && (computerSelection.toUpperCase() === "Paper")){
+        score += 1;
         return "You Win! Scissors beats Paper"
-    }
+    }  
+
 }
 
-const playerSelection = prompt("Rock,Paper,Scissors!"," ");
-//const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+// const playerSelection = prompt("Rock,Paper,Scissors!"," ");
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+//while (comScore<3 && score<3){
+for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Rock,Paper,Scissors!"," ");
+    const computerSelection = getComputerChoice();
 
-function game(){
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(score, comScore)
     
 }
+if (score > comScore){
+    console.log("You Win the Match!")
+}else if (score < comScore){
+    console.log("You Lose the Match!")
+}else{
+    console.log("It's a Draw Game!")
+}
+//} 
+
+
+
 
